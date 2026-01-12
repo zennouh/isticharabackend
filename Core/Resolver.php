8899e7b67935a1222f9da7772624d7bffa7a74c8
nouh <nouh.nowile@gmail.com>
2026-01-12 17:08:10 +0100

    create the resolver for excute the method that come from the router class

---
+++ Core/Resolver.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Core;
+
+use Core\Http\Request;
+use Core\Http\Response;
+use Core\Services\InjectionService;
+use Core\Services\InjectService;
+use ReflectionMethod;
+
+class Resolver
+{
+
+
+    public function resolve(array $action)
+    {
+
+
+        $controller = InjectionService::inject($action['class']);
+
+        $reflection  = new ReflectionMethod($controller, $action['method']);
+        $dependencies = [];
+
+        foreach ($reflection->getParameters() as $param) {
+            $type = $param->getType();
+            if ($type && $type->getName() === Request::class) {
+
+                $dependencies[] = App::getContainer()->resolve(Request::class);
+                
+            } elseif ($type && $type->getName() === Response::class) {
+
+                $dependencies[] = App::getContainer()->resolve(Response::class);
+
+            } elseif (!empty($action['params'])) {
+
+                $dependencies[] = array_shift($action['params']);
+
+            }
+        }
+
+        return $reflection->invokeArgs($controller, $dependencies);
+    }
+}
