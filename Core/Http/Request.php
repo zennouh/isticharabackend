4616d0d35538bd5e8cc37880caf513d142f5e86e
nouh <nouh.nowile@gmail.com>
2026-01-12 16:41:34 +0100

    create the structer of the project

---
+++ Core/Http/Request.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Core\Http;
+
+use ObjectMapper;
+
+class Request
+
+{
+
+    public function getMethod()
+    {
+        $method = $_SERVER["REQUEST_METHOD"];
+        $__methodIsSet = isset($_POST["__method"]);
+        if ($method == "POST" && $__methodIsSet) {
+            return $_POST["__method"];
+        }
+        return $_SERVER["REQUEST_METHOD"];
+    }
+
+    public function getUri()
+    {
+        return parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+    }
+
+    public function getQueryParams()
+    {
+        $query = parse_url($_SERVER["REQUEST_URI"])["query"];
+        return $query;
+    }
+
+    public function getPostParams(): array
+    {
+        return $_POST;
+    }
+
+    public function getGetParamsAsObj(string $className): object
+    {
+        $params = ObjectMapper::arrayToObject($className, $_GET);
+
+        return $params;
+    }
+}
---
+++ Core/Http/Response.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace Core\Http;
+
+class Response
+{
+
+    static function redirect($location, $statusCode  = 200)
+    {
+        http_response_code($statusCode);
+        header("location: $location");
+        exit;
+    }
+}
