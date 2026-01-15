284d70170bd81ec4e2740a519f7829ca74fda01c
nouh <nouh.nowile@gmail.com>
2026-01-15 16:54:21 +0100

    add the name of columns in the ville entity

--- App/Entity/Ville.php
+++ App/Entity/Ville.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use DateTimeImmutable;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 
 #[ORM\Entity]
 #[ORM\Table(name: 'villes')]
@@ -21,20 +23,30 @@ class Ville
     #[ORM\Column(type: 'string', length: 255)]
     private string $region;
 
-    #[ORM\Column(type: 'string', length: 255)]
+    #[ORM\Column(type: 'string', length: 255, name: "code_postale")]
     private string $codePostale;
 
-    #[ORM\Column(type: 'datetime_immutable')]
+    #[ORM\Column(type: 'datetime_immutable', name:"create_at")]
     private DateTimeImmutable $createAt;
+
+
+    #[ORM\OneToMany(mappedBy: 'ville', targetEntity: Avocat::class, cascade: ['persist', 'remove'])]
+    private Collection $avocats;
 
     public function __construct()
     {
         $this->createAt = new DateTimeImmutable();
+        $this->avocats = new ArrayCollection();
     }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
     }
 
     public function getName(): string
@@ -74,4 +86,29 @@ class Ville
     {
         return $this->createAt;
     }
+
+
+    public function getAvocats(): Collection
+    {
+        return $this->avocats;
+    }
+
+    public function addAvocat(Avocat $avocat): self
+    {
+        if (!$this->avocats->contains($avocat)) {
+            $this->avocats->add($avocat);
+            $avocat->setVille($this); // keep inverse side updated
+        }
+        return $this;
+    }
+
+    // public function removeAvocat(Avocat $avocat): self
+    // {
+    //     if ($this->avocats->removeElement($avocat)) {
+    //         if ($avocat->getVille() === $this) {
+    //             $avocat->setVille(null);
+    //         }
+    //     }
+    //     return $this;
+    // }
 }
