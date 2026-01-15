e38952235b09122a3652546805b1c5d00187ddd7
nouh <nouh.nowile@gmail.com>
2026-01-15 16:34:30 +0100

    edit the inject function and namespaces

--- App/Core/Services/InjectionService.php
+++ App/Core/Services/InjectionService.php
@@ -1,21 +1,31 @@
 <?php
 
-namespace Core\Services;
+namespace App\Core\Services;
 
+ 
 use ReflectionClass;
 
 class InjectionService
 {
-    static function inject(string $class)
+    static function inject(string $class): object
     {
+        // avocta controller
         $reflection = new ReflectionClass($class);
         $constructor = $reflection->getConstructor();
+        // var_dump($constructor);
+        if (!$constructor) {
+            return $reflection->newInstance();;
+        }
         $params = $constructor->getParameters();
+
+        // var_dump($params);
         $dependencies  = [];
         foreach ($params as $param) {
             $type = $param->getType();
-            $className = $type->getName();
+            $className = $type->getName(); // repo
+            // echo $className;
             $classReflection = new ReflectionClass($className);
+            // echo "<br>" . "iurgeuiq"  . "<br>"; 
             $object = $classReflection->newInstance();
             $dependencies[] = $object;
         }
