c0a277ff3b3660742358f70250ba8b9c75a8b026
nouh <nouh.nowile@gmail.com>
2026-01-15 16:31:37 +0100

    initilize the class container

--- App/Core/bootstrap.php
+++ App/Core/bootstrap.php
@@ -6,12 +6,15 @@ use App\Core\Services\Container;
 use App\Core\App;
 use App\Core\Http\Request;
 use App\Core\Http\Response;
- 
+use App\Core\MyEntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 
 $container = new Container();
 
 $container->bind(Request::class, fn() => new Request());
 
 $container->bind(Response::class, fn() => new Response());
+
+// $container->bind(EntityManagerInterface::class, fn() => new MyEntityManager());
 
 App::setContainer($container);
