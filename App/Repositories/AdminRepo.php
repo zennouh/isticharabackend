c930fe050a52e47e2b13cc77a744d08a0f7599fa
nouh <nouh.nowile@gmail.com>
2026-01-15 16:41:35 +0100

    create admin repo

--- App/Repositories/AdminRepo.php
+++ App/Repositories/AdminRepo.php
@@ -1,58 +1,18 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repositories;
 
+use App\Core\MyEntityManager;
 use App\Entity\Admin;
-use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\EntityManagerInterface;
+ 
 
-class AdminRepo extends EntityRepository
+class AdminRepo extends BaseRepository
 {
-    private EntityManagerInterface $em;
 
-    public function __construct(EntityManagerInterface $em)
-    {
-        parent::__construct($em, $em->getClassMetadata(Admin::class));
-        $this->em = $em;
-    }
 
-    // Example: Find by speciality
-    public function findBySpecialite(string $specialite): array
+    public function __construct()
     {
-        // return $this->createQueryBuilder('a')
-        //     ->where('a.specialite = :specialite')
-        //     ->setParameter('specialite', $specialite)
-        //     ->getQuery()
-        //     ->getResult();
-        return [];
-    }
-
-    // Example: Find online lawyers
-    public function findConsultEnLigne(): array
-    {
-        // return $this->createQueryBuilder('a')
-        //     ->where('a.consultEnLigne = :yes')
-        //     ->setParameter('yes', 'yes')
-        //     ->getQuery()
-        //     ->getResult();
-        return [];
-    }
-
-    // Save an Admin
-    public function save(Admin $admin, bool $flush = true): void
-    {
-        $this->em->persist($admin);
-        if ($flush) {
-            $this->em->flush();
-        }
-    }
-
-    // Delete an Admin
-    public function remove(Admin $admin, bool $flush = true): void
-    {
-        $this->em->remove($admin);
-        if ($flush) {
-            $this->em->flush();
-        }
+        $this->em = MyEntityManager::get();
+        $this->entityClass = Admin::class;
     }
 }
