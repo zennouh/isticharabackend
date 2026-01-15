79f95ca21f2bc65b0afc111b56c984278c0efd26
nouh <nouh.nowile@gmail.com>
2026-01-15 16:28:10 +0100

    finish avocat controller with its all methods

--- App/Controllers/AvocatController.php
+++ App/Controllers/AvocatController.php
@@ -2,8 +2,13 @@
 
 namespace App\Controllers;
 
-use App\Repository\AvocatRepo;
-use Core\Router\Attributes\Route;
+use App\Core\Http\Request;
+use App\Core\Router\Attributes\Route;
+use App\Core\Services\JsonResponse;
+use App\Core\Services\ObjectMapper;
+use App\Entity\Avocat;
+use App\Repositories\AvocatRepo;
+
 
 class AvocatController
 {
@@ -11,10 +16,71 @@ class AvocatController
     public function __construct(private AvocatRepo $avocatRepo) {}
 
 
-    #[Route(name: "/avocats", methods: "GET")]
+    #[Route("/avocats", "GET")]
     public function index()
     {
-        // $data = $this->avocatRepo->findAll();
-        echo "all avocats";
+        $resultArray = [];
+        $avocats = $this->avocatRepo->findAll();
+
+
+        foreach ($avocats as $av) {
+            $resultArray[]  = ObjectMapper::normalizer($av);
+        }
+
+
+        $response  = new JsonResponse([
+            'success' => true,
+            'data' => $resultArray,
+        ], 200);
+
+        $response->send();
+    }
+
+    #[Route("/avocats", "POST")]
+    public function store(Request $request)
+    {
+
+        $avocat = $request->getPostParams(Avocat::class);
+
+        $this->avocatRepo->save($avocat);
+        $response  = new JsonResponse([
+            'success' => true,
+            'message' => 'Avocat created',
+        ], 201);
+        $response->send();
+    }
+
+    #[Route("/avocats/{id}", "PATCH")]
+    public function update(Request $request, int $id): void
+    {
+        $avocatArray = $request->bodyParam();
+
+        $avocatObject = $this->avocatRepo->find($id);
+
+        $obj = ObjectMapper::updateObject($avocatObject, $avocatArray);
+
+        $this->avocatRepo->update($obj);
+
+
+        $response  = new JsonResponse([
+            'success' => true,
+            'message' => 'Avocat Has been updated',
+
+        ], 201);
+
+        $response->send();
+    }
+
+
+    #[Route("/avocats/{id}", "DELETE")]
+    public function destroy(int $id)
+    {
+        $this->avocatRepo->delete(Avocat::class, $id);
+        $response  = new JsonResponse([
+            'success' => true,
+            'message' => 'Avocat has been deleted',
+        ], 200);
+
+        $response->send();
     }
 }
