225dd5929d7f96ab95f8e961c47424c053dde4b5
nouh <nouh.nowile@gmail.com>
2026-01-15 16:29:32 +0100

    the enhence on the index.php has end

--- public/index.php
+++ public/index.php
@@ -2,15 +2,40 @@
 require_once '../vendor/autoload.php';
 require_once "../App/Core/bootstrap.php";
 
-use Core\Kernel;
+use App\Core\Kernel;
+use App\Core\Services\JsonResponse;
+
+
+set_error_handler(function (
+    $err_severity,
+    $err_msg,
+    $err_file,
+    $err_line,
+    $err_context
+) {
+    throw new ErrorException(
+        $err_msg,
+        500,
+        $err_severity,
+        filename: $err_file,
+        line: $err_line
+    );
+});
+
 
 try {
 
     $kernel = new Kernel();
     $kernel->handler();
 } catch (Throwable $e) {
+ 
 
-    
-    echo $e->getTraceAsString() . "<br>";
-    
+    $response  = new JsonResponse([
+        'success' => false,
+        'errno'=>  $e->getCode(),
+        'message' => $e->getMessage(),
+        'track' => $e->getTraceAsString(),
+    ], 500);
+
+    $response->send();
 }
