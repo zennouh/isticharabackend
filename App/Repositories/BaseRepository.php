44b839000df945ed80357940279aa74eac81f8f6
nouh <nouh.nowile@gmail.com>
2026-01-15 16:52:23 +0100

    edit the base repo with new methods

---
+++ App/Repositories/BaseRepository.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Core\Services\ObjectMapper;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+
+abstract class BaseRepository
+{
+    protected EntityManagerInterface $em;
+    protected string $entityClass;
+    protected string $alias = 'e';
+
+
+
+    protected function qb(): QueryBuilder
+    {
+        return $this->em->createQueryBuilder()
+            ->select($this->alias)
+            ->from($this->entityClass, $this->alias)
+
+        ;
+    }
+
+
+    public function find(int $id): ?object
+    {
+        return $this->qb()
+            ->where("{$this->alias}.id = :id")
+            ->setParameter('id', $id)
+
+            ->getQuery()
+            ->getOneOrNullResult();
+    }
+
+
+    public function findAll(): array
+    {
+        return $this->qb()
+            ->getQuery()
+            ->getResult(); // ENTITY objects
+    }
+
+    public function findBy(array $criteria, ?int $limit = null, ?int $offset = null): array
+    {
+        $qb = $this->qb();
+
+        foreach ($criteria as $field => $value) {
+            $qb->andWhere("{$this->alias}.$field = :$field")
+                ->setParameter($field, $value);
+        }
+
+        if ($limit !== null) {
+            $qb->setMaxResults($limit);
+        }
+
+        if ($offset !== null) {
+            $qb->setFirstResult($offset);
+        }
+
+        return $qb->getQuery()->getResult(); // ENTITY objects
+    }
+
+
+    public function findOneBy(array $criteria): ?object
+    {
+        return $this->findBy($criteria, 1)[0] ?? null;
+    }
+
+    public function save(object $entity, bool $flush = true): void
+    {
+        $this->em->persist($entity);
+
+        if ($flush) {
+            $this->em->flush();
+        }
+    }
+    public function update(object $entity, bool $flush = true): void
+    {
+        
+       
+
+        $this->em->persist($entity);
+
+        if ($flush) {
+            $this->em->flush();
+        }
+    }
+    public function delete(string $entity, int $id): void
+    {
+        $avocat = $this->em->find($entity, $id);
+
+        if ($avocat) {
+            $this->em->remove($avocat);
+            $this->em->flush();
+        }
+    }
+}
